362b0f23de06f7e8b57271d2799d2303f09c767b
Andrius <andrius.bendoraitis1985@gmail.com>
2024-08-06 07:34:48 +0300

    create config class for JpaAuditing config

--- src/main/java/andrius/bendoraitis/mini/bank/system/MiniBankSystemApplication.java
+++ src/main/java/andrius/bendoraitis/mini/bank/system/MiniBankSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MiniBankSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MiniBankSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MiniBankSystemApplication.class, args);
+    }
 
 }
